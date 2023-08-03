71a41bcb24efd3a41e18a44db8fa4fe13992eada
dima.avdeev <dima.avdeev@jetbrains.com>
2023-08-03 11:33:02 +0400

    reproduce

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,9 +1,43 @@
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun App() {
-    UserHead("SS", Color.LightGray)
+    MaterialTheme {
+        Box(modifier = Modifier.fillMaxSize(), Alignment.Center) {
+            CircleWithText()
+        }
+    }
 }
 
-expect fun getPlatformName(): String
+@Composable
+fun CircleWithText(
+    modifier: Modifier = Modifier,
+    size: Dp = 24.dp,
+) {
+    Box(modifier.size(size), contentAlignment = Alignment.Center) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawCircle(SolidColor(Color.Gray))
+        }
+        Text(
+            text = "AB",
+            color = Color.White,
+            fontSize = 10.sp,
+            modifier = Modifier.fillMaxSize(),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
